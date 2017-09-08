9bc2c3decaa64e8d72ab02274642e56e1dc405bf
Ryo SUETSUGU <suetsugu.r@gmail.com>
2017-09-08 12:56:46 +0900

    Removed: rails-observers

---
+++ app/models/concerns/replace_url.rb
@@ -0,0 +1,41 @@
+module ReplaceURL
+# CREATE DIR Manually
+# KNOWN Problem: Changing BitBucket Git Dir in use
+# KNOWN Problem: Two Repositorys with the same name from different users
+
+  extend ActiveSupport::Concern 
+
+  included do
+    before_create :replace_url
+  end
+
+  def replace_url
+    flag = type == 'Git' || self.type == 'Repository::Git'
+    flag &= self.url.match('.*(bitbucket.org|github.com).*') 
+    if flag 
+      base_dir_name = self.url[/[^\/]+.git/]
+      url = self.url
+      user = /^\S*[:\/](.*)\/\S*$/.match( url )
+      user = user[1]
+      git_dir = Setting.plugin_redmine_bitbucketgit_hook[:bitbucketgit_dir].to_s
+      git_dir = git_dir  + '/' + user + '_' + base_dir_name 
+      redminedir = Dir.getwd + '/'
+      comm_str = ""
+      unless Dir[redminedir+git_dir] == []
+        comm_str = 'rm -rf "' + redminedir + git_dir + '" &&'
+      end
+      comm_str += 'git clone --mirror '+ url + ' "'+ redminedir + git_dir +'"'
+      b = system(comm_str)
+      self.url = redminedir + git_dir
+      return false
+    end
+  end
+  
+  private
+  def exec(command)
+    Rails.logger.info("BitbucketGitHook: Executing command: '#{command}'")
+    output = `#{command}`
+    Rails.logger.info("BitbucketGitHook: Shell returned '#{output}'")
+  end
+ 
+end
--- init.rb
+++ init.rb
@@ -1,7 +1,10 @@
 require 'redmine'
-require_dependency File.expand_path(File.join(File.dirname(__FILE__), 'app/models/repository_observer'))
+require_dependency File.expand_path(File.join(File.dirname(__FILE__), 'app/models/concerns/replace_url'))
 
-Rails.logger.info "register redmine_bitbucketgit_hook"
+Repository.class_eval do
+  include ReplaceURL
+end
+
 Redmine::Plugin.register :redmine_bitbucketgit_hook do
   name 'Redmine Bitbucket GIT Hook plugin'
   author 'Bastian Bringenberg'
@@ -12,8 +15,3 @@ Redmine::Plugin.register :redmine_bitbucketgit_hook do
            },
            :partial => 'settings/bitbucketgit_hook_setting')
 end
-
-#Rails.configuration.active_record.observers << :repository_observer
-ActiveRecord::Base.observers << :repository_observer
-
-Rails.logger.info "redmine_bitbucketgit_hook end"
